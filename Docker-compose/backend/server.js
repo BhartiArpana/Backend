94115a5c46766c08f6157b1229561e79a67be19c
Arpana bharti <arpanabharti09@gmail.com>
2026-08-22 14:56:26 +0530

    feat - docker implementation with single image for frontend and backend

--- Docker-compose/backend/server.js
+++ Docker-compose/backend/server.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -17,6 +18,10 @@ app.get("/api/users", (req, res) => {
     { id: 4, name: "Bob Brown" },
   ];
   res.status(200).json(users);
+});
+
+app.get('*name', (req, res) => {
+  res.sendFile('public/index.html', { root: __dirname });
 });
 
 app.listen(3000, () => {
